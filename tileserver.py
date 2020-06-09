8f318643050a800c1bc0504e780a333f8cfc44cb
Luiz <motta.luiz@gmail.com>
2020-06-09 12:32:08 -0300

    Addex license

--- tileserver.py
+++ tileserver.py
@@ -1,3 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+"""
+***********************************************************************************
+Name                 : Tiles server                                               *
+Description          : Example in Flask for use the Tiles Raster script.          *
+Date                 : June, 2020                                                 *
+copyright            : (C) 2020 by Luiz Motta                                     *
+email                : motta.luiz@gmail.com                                       *
+***********************************************************************************
+
+***********************************************************************************
+*                                                                                 *
+*   This code is licensed under an MIT/X style license with the following terms:  *
+*                                                                                 *
+*   Copyright (c) 2020 Luiz Motta                                                 *
+*                                                                                 *
+*   Permission is hereby granted, free of charge, to any person obtaining a copy  *
+*   of this software and associated documentation files (the "Software"), to deal *
+*   in the Software without restriction, including without limitation the rights  *
+*   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     *
+*   copies of the Software, and to permit persons to whom the Software is         *
+*   furnished to do so, subject to the following conditions:                      *
+*                                                                                 *
+*   The above copyright notice and this permission notice shall be included in    *
+*   all copies or substantial portions of the Software.                           *
+*                                                                                 *
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    *
+*   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      *
+*   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   *
+*   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        *
+*   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, *
+*   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN     *
+*   THE SOFTWARE.                                                                 *
+*                                                                                 *
+***********************************************************************************
+"""
+
+__author__ = 'Luiz Motta'
+__date__ = '2020-06-09'
+__copyright__ = '(C) 2020, Luiz Motta'
+__revision__ = '$Format:%H$'
+
+
 import os, functools
 
 from flask import Flask, Response, render_template
@@ -13,23 +57,17 @@ STATUS_HTTP_TILES_RASTER = {
     2: 400  # Bad Request
 }
 
-IMAGES_DIR = '/vsis3/lmottadata/'
+IMAGES_DIR = '/home/lmotta/data/db/drone'
 catalogRaster = {
-    'drone': { 'file': 'drone.tif', 'tilesraster': None }
+    'ac': { 'file': 'ac.tif', 'tilesraster': None }
 }
-
-#IMAGES_DIR = '/home/lmotta/data/db/images/landsat'
-#catalogRaster = {
-    #'15293': { 'file': 'LC82320682015293LGN00_r6g5b4.tif', 'tilesraster': None },
-    #'13166': { 'file': 'LC82330682013166LGN00_r6g5b4.tif', 'tilesraster': None }
-#}
 
 def responseError(message, status):
     return Response( f"{message}\n", status, mimetype='text/plain' )
 
 @app.route('/demo')
 def demo():
-    return render_template('demo_leaflet.html')
+    return render_template('demo_openlayers_drone.html')
 
 @app.route('/')
 def index():
--- tilesraster.py
+++ tilesraster.py
@@ -1,4 +1,46 @@
 # -*- coding: utf-8 -*-
+#
+"""
+***********************************************************************************
+Name                 : Tiles raster                                               *
+Description          : Create a tile(PNG) from parameters: zoom, xtile, ytile.    *
+Date                 : June, 2020                                                 *
+copyright            : (C) 2020 by Luiz Motta                                     *
+email                : motta.luiz@gmail.com                                       *
+***********************************************************************************
+
+***********************************************************************************
+*                                                                                 *
+*   This code is licensed under an MIT/X style license with the following terms:  *
+*                                                                                 *
+*   Copyright (c) 2020 Luiz Motta                                                 *
+*                                                                                 *
+*   Permission is hereby granted, free of charge, to any person obtaining a copy  *
+*   of this software and associated documentation files (the "Software"), to deal *
+*   in the Software without restriction, including without limitation the rights  *
+*   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     *
+*   copies of the Software, and to permit persons to whom the Software is         *
+*   furnished to do so, subject to the following conditions:                      *
+*                                                                                 *
+*   The above copyright notice and this permission notice shall be included in    *
+*   all copies or substantial portions of the Software.                           *
+*                                                                                 *
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    *
+*   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      *
+*   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   *
+*   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        *
+*   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, *
+*   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN     *
+*   THE SOFTWARE.                                                                 *
+*                                                                                 *
+***********************************************************************************
+"""
+
+__author__ = 'Luiz Motta'
+__date__ = '2020-06-09'
+__copyright__ = '(C) 2020, Luiz Motta'
+__revision__ = '$Format:%H$'
+
 
 import os, math, collections
 
